5ee4062c34e2634f66e00514613058b5de9e5537
Palak <rpalak317@gmail.com>
2024-07-16 15:18:28 +0530

    http methods

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-const http =require('http');
-const fs =require('fs');
+const http = require("http");
+const fs = require("fs");
 const url = require("url");
 
-const MyServer=http.createServer((req,res)=>{
-    if(req.url ==="/favicon.ico")return res.end();
-       const log =`${Date.now()} :${req.url} New Request received\n`;
-       const myUrl = url.parse(req.url,true);
-       console.log(myUrl);
-       fs.appendFile("log.txt",log,(err,data)=>{
-        switch(myUrl.pathname){
-            case "/": res.end("Home Page ")
-            break;
-            case "/about": 
-            const username = myUrl.query.myname
-            res.end(  `This is about page and hi ${username}`)
-            break;
-            default: res.end("404 error ... OPPS page not found ")
+const MyServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()} :${req.method}   ${
+    req.url
+  } New Request received\n`;
+  const myUrl = url.parse(req.url, true);
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        if (req.method === "GET") res.end("This is a about page");
+        break;
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`This is about page and hi ${username}`);
+        break;
+      case "/signup":
+        if (req.method === "GET") res.end("This is a sign up page ");
+        else if (req.method === "POST ") {
+          //DB QUERY
+          res.end("Success");
         }
-       })
-})
+        break;
+      default:
+        res.end("404 error ... OPPS page not found ");
+    }
+  });
+});
 
-MyServer.listen(4000,()=>{
-    console.log("My server is started")
-})
+MyServer.listen(4000, () => {
+  console.log("My server is started");
+});
