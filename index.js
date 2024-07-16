8df34dff894425e022dfcd190ca35823d6c99748
Palak <rpalak317@gmail.com>
2024-07-16 15:01:41 +0530

    url methods

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const http =require('http');
 const fs =require('fs');
+const url = require("url");
 
 const MyServer=http.createServer((req,res)=>{
+    if(req.url ==="/favicon.ico")return res.end();
        const log =`${Date.now()} :${req.url} New Request received\n`;
+       const myUrl = url.parse(req.url,true);
+       console.log(myUrl);
        fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/": res.end("Home Page ")
             break;
-            case "/about": res.end("This is about page and my name is Palak")
+            case "/about": 
+            const username = myUrl.query.myname
+            res.end(  `This is about page and hi ${username}`)
             break;
             default: res.end("404 error ... OPPS page not found ")
         }
