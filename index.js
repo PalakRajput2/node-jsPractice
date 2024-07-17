943db7e89871f42aab937394fb219d52f8674d4f
Palak <rpalak317@gmail.com>
2024-07-17 12:04:31 +0530

    express started

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express =require("express");
 
-const MyServer = http.createServer((req, res) => {
-  if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()} :${req.method}   ${
-    req.url
-  } New Request received\n`;
-  const myUrl = url.parse(req.url, true);
-  fs.appendFile("log.txt", log, (err, data) => {
-    switch (myUrl.pathname) {
-      case "/":
-        if (req.method === "GET") res.end("This is a about page");
-        break;
-      case "/about":
-        const username = myUrl.query.myname;
-        res.end(`This is about page and hi ${username}`);
-        break;
-      case "/signup":
-        if (req.method === "GET") res.end("This is a sign up page ");
-        else if (req.method === "POST ") {
-          //DB QUERY
-          res.end("Success");
-        }
-        break;
-      default:
-        res.end("404 error ... OPPS page not found ");
-    }
-  });
+const app =express();
+
+app.get("/",(req,res)=>{
+  res.send("Hello from Home page ");
+});
+app.get("/about",(req,res)=>{
+  res.send("Hello from About page " );
 });
 
-MyServer.listen(4000, () => {
+
+app.listen(4000, () => {
   console.log("My server is started");
 });
